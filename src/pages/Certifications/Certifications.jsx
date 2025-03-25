47452777db9e625e9aa860082902ccea037d16cc
Alejandro Infante <alejandroinfante.ga@gmail.com>
2025-03-25 08:58:48 -0600

    Se agregan imagenes de certificaciones

--- src/pages/Certifications/Certifications.jsx
+++ src/pages/Certifications/Certifications.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import NavBar from '../../components/navBar/NavBar';
 import Footer from '../../components/footer/Footer'
 import LandingLayout from '../../layout/landingLayout/LandingLayout';
+import ISO9001 from '../../assets/ISO9001.png';
+import ISO14001 from '../../assets/ISO14001.png';
+import ISO27001 from '../../assets/ISO27001.png';
 import './style.css'
 
 function Certifications(props) {
@@ -14,7 +17,7 @@ function Certifications(props) {
                     <div>
                         <div className='test'>
                             <div className='info-image'>
-
+                                <img src={ISO9001}></img>
                             </div>
                             <div className='info-data'>
                                 <h3>Titulo</h3>
@@ -31,7 +34,7 @@ function Certifications(props) {
                         </div>
                         <div className='test'>
                             <div className='info-image'>
-
+                                <img src={ISO27001}></img>
                             </div>
                             <div className='info-data'>
                                 <h3>Titulo</h3>
@@ -46,7 +49,23 @@ function Certifications(props) {
                                 </p>
                             </div>
                         </div>
-                        
+                        <div className='test'>
+                            <div className='info-image'>
+                                <img src={ISO14001}></img>
+                            </div>
+                            <div className='info-data'>
+                                <h3>Titulo</h3>
+                                <p>
+                                    data data data data data data data data data data data
+                                    data data data data data data data data data data data
+                                    data data data data data data data data data data data
+                                    data data data data data data data data data data data
+                                    data data data data data data data data data data data
+                                    data data data data data data data data data data data
+                                    data data data data data data data data data data data
+                                </p>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,9 @@ import NavBar from '../../components/navBar/NavBar';
 import Footer from '../../components/footer/Footer'
 import LandingLayout from '../../layout/landingLayout/LandingLayout';
 import homeImg from '../../assets/image.png'
+import ISO9001 from '../../assets/ISO9001.png';
+import ISO14001 from '../../assets/ISO14001.png';
+import ISO27001 from '../../assets/ISO27001.png';
 import './style.css'
 
 function Home(props) {
@@ -67,9 +70,9 @@ function Home(props) {
                     </div>
                     <div className='derecha'>
                         <h2>Certificaciones</h2>
-                        <div className='certificacion'></div>
-                        <div className='certificacion'></div>
-                        <div className='certificacion'></div>
+                        <div className='certificacion'> <img src={ISO9001}></img></div>
+                        <div className='certificacion'> <img src={ISO27001}></img></div>
+                        <div className='certificacion'> <img src={ISO14001}></img></div>
                     </div>
                 </div>
             </div>
